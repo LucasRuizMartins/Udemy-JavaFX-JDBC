3d1284394c53b8b9c7ed895b4560d911310dfe9a
Lucas <deth_lucas@hotmail.com>
2022-02-27 11:17:28 -0300

    Validation exception

--- src/gui/DepartmentFormController.java
+++ src/gui/DepartmentFormController.java
@@ -3,7 +3,9 @@ package gui;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 import java.util.ResourceBundle;
+import java.util.Set;
 
 import db.DbException;
 import gui.listeners.DataChangeListener;
@@ -18,6 +20,7 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import model.entities.Department;
+import model.exception.ValidationException;
 import model.services.DepartmentService;
 
 public class DepartmentFormController implements Initializable {
@@ -50,14 +53,14 @@ public class DepartmentFormController implements Initializable {
 		this.service = service;
 
 	}
+
 	private List<DataChangeListener> dataChangeListeners = new ArrayList<>();
-	
-	
-	public void subscribeDataChangeListener (DataChangeListener listener) {
+
+	public void subscribeDataChangeListener(DataChangeListener listener) {
 		dataChangeListeners.add(listener);
-		
+
 	}
-	
+
 	@FXML
 	private void onbtSaveAction(ActionEvent event) {
 		if (entity == null) {
@@ -73,8 +76,13 @@ public class DepartmentFormController implements Initializable {
 			entity = getFormData();
 			service.saveOrUpdate(entity);
 			notifyDataChangeListeners();
-			Utils.currentStage(event).close();;
-		} 
+			Utils.currentStage(event).close();
+			;
+		}
+		catch(ValidationException e) {
+			setErrorMessages(e.getErrors());
+			
+		}
 		catch (DbException e) {
 			Alerts.showAlert("Error Saving OBJ", null, e.getMessage(), AlertType.ERROR);
 
@@ -84,21 +92,34 @@ public class DepartmentFormController implements Initializable {
 	private void notifyDataChangeListeners() {
 		for (DataChangeListener listener : dataChangeListeners) {
 			listener.onDataChanged();
-			
+
 		}
-		
+
 	}
 
 	private Department getFormData() {
 		Department obj = new Department();
+
+		ValidationException exception = new ValidationException("Validation Error");
+
 		obj.setId(Utils.tryParseToInt(txtId.getText()));
+
+		if (txtName.getText() == null || txtName.getText().trim().equals("")) {
+			exception.addError("name", "field can't be empty");
+
+		}
 		obj.setName(txtName.getText());
+
+		if (exception.getErrors().size() > 0) {
+			throw exception;
+		}
 		return obj;
 	}
 
 	@FXML
 	private void onbtCancelAction(ActionEvent event) {
-		Utils.currentStage(event).close();;
+		Utils.currentStage(event).close();
+		;
 
 	}
 
@@ -124,5 +145,12 @@ public class DepartmentFormController implements Initializable {
 
 	}
 
+	private void setErrorMessages(Map<String, String> errors) {
+		Set<String> fields = errors.keySet();
 
+		if (fields.contains("name")) {
+			LabelErrorName.setText(errors.get("name"));
+
+		}
+	}
 }
